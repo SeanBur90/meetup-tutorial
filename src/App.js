eeb91aa29357c10cbe4e05d48194c50570d7959a
Sean Burnett <seanburnett@Seans-MacBook-Pro.local>
2021-08-15 01:03:32 -0400

    Second commit

--- src/App.js
+++ src/App.js
@@ -3,22 +3,24 @@ import AllMeetupsPage from "./pages/AllMeetups";
 import NewMeetupPage from "./pages/NewMeetup";
 import FavoritesPage from "./pages/Favorites";
 import MainNavigation from "./components/layout/MainNavigation";
+import Layout from "./components/layout/Layout";
 
 function App() {
   return (
     <div>
-      <MainNavigation />
-      <Switch>
-        <Route exact path="/">
-          <AllMeetupsPage />
-        </Route>
-        <Route path="/new-meetup">
-          <NewMeetupPage />
-        </Route>
-        <Route path="/favorites">
-          <FavoritesPage />
-        </Route>
-      </Switch>
+      <Layout>
+        <Switch>
+          <Route exact path="/">
+            <AllMeetupsPage />
+          </Route>
+          <Route path="/new-meetup">
+            <NewMeetupPage />
+          </Route>
+          <Route path="/favorites">
+            <FavoritesPage />
+          </Route>
+        </Switch>
+      </Layout>
     </div>
   );
 }
--- src/components/meetups/MeetupItem.js
+++ src/components/meetups/MeetupItem.js
@@ -1,21 +1,23 @@
 import classes from "./MeetupItem.module.css";
-import pic from "./download.jpeg";
-import pic2 from "./downloadd.jpeg";
+
+import Card from "../UI/Card";
 
 function MeetupItem(props) {
   return (
     <li className={classes.item}>
-      <div className={classes.image}>
-        <img src={props.image} alt="" />
-      </div>
-      <div className={classes.content}>
-        <h3>{props.title}</h3>
-        <address>{props.address}</address>
-        <p>{props.description}</p>
-      </div>
-      <div className={classes.actions}>
-        <button>To Favoites</button>
-      </div>
+      <Card>
+        <div className={classes.image}>
+          <img src={props.image} alt={props.title} />
+        </div>
+        <div className={classes.content}>
+          <h3>{props.title}</h3>
+          <address>{props.address}</address>
+          <p>{props.description}</p>
+        </div>
+        <div className={classes.actions}>
+          <button>To Favoites</button>
+        </div>
+      </Card>
     </li>
   );
 }
