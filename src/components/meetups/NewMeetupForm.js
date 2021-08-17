70a5f5198bb4a3cd4febbc8183a8e5ffee8224d7
Sean Burnett <seanburnett@Seans-MacBook-Pro.local>
2021-08-16 20:12:27 -0400

    form page update

--- src/components/meetups/NewMeetupForm.js
+++ src/components/meetups/NewMeetupForm.js
@@ -1,14 +1,52 @@
 import Card from "../UI/Card";
 import classes from "./NewMeetupForm.module.css";
+import { useRef } from "react";
 
 function NewMeetupForm() {
+  const titleInputRef = useRef();
+  const imageInputRef = useRef();
+  const addressInputRef = useRef();
+  const descriptionInputRef = useRef();
+
+  function submitHandler(e) {
+    e.preventDefault();
+
+    const enteredTitle = titleInputRef.current.value;
+    const enteredImage = imageInputRef.current.value;
+    const enteredAddress = addressInputRef.current.value;
+    const enteredDescription = descriptionInputRef.current.value;
+
+    const meetupData = {
+      title: enteredTitle,
+      image: enteredImage,
+      address: enteredAddress,
+      description: enteredDescription,
+    };
+
+    console.log(meetupData);
+  }
+
   return (
     <Card>
-      <form className={classes.form}>
+      <form className={classes.form} onSubmit={submitHandler}>
         <div className={classes.control}>
-          <label> Meetup Title</label>
-          <input type="text" required id="title" />
+          <label htmlFor="title"> Meetup Title</label>
+          <input type="text" required id="title" ref={titleInputRef} />
         </div>
+        <div className={classes.control}>
+          <label htmlFor="image">Meetup Image</label>
+          <input type="url" required id="image" ref={imageInputRef} />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="address">Address</label>
+          <input type="text" required id="address" ref={addressInputRef} />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="description">Meetup Image</label>
+          <textarea type="text" required rows="5" ref={descriptionInputRef} />
+        </div>
+        <div classNam={classes.actions} e></div>
+        <button>Add Meetup</button>
       </form>
     </Card>
   );
