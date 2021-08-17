3c64fc712e4387055932de44b1745086f733fc2e
Sean Burnett <seanburnett@Seans-MacBook-Pro.local>
2021-08-16 22:31:15 -0400

    update

--- src/components/meetups/NewMeetupForm.js
+++ src/components/meetups/NewMeetupForm.js
@@ -2,7 +2,7 @@ import Card from "../UI/Card";
 import classes from "./NewMeetupForm.module.css";
 import { useRef } from "react";
 
-function NewMeetupForm() {
+function NewMeetupForm(props) {
   const titleInputRef = useRef();
   const imageInputRef = useRef();
   const addressInputRef = useRef();
@@ -23,7 +23,7 @@ function NewMeetupForm() {
       description: enteredDescription,
     };
 
-    console.log(meetupData);
+    props.onAddMeetup(meetupData);
   }
 
   return (
@@ -45,7 +45,7 @@ function NewMeetupForm() {
           <label htmlFor="description">Meetup Image</label>
           <textarea type="text" required rows="5" ref={descriptionInputRef} />
         </div>
-        <div classNam={classes.actions} e></div>
+        <div className={classes.actions} e></div>
         <button>Add Meetup</button>
       </form>
     </Card>
--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,10 +1,28 @@
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
+import { useHistory } from "react-router-dom";
 
 function NewMeetupPage() {
+  const history = useHistory();
+
+  function addMeetupHandler(meetupData) {
+    fetch(
+      "https://react-getting-started-8024f-default-rtdb.firebaseio.com/.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    ).then(() => {
+      history.replace("/");
+    });
+  }
+
   return (
     <section>
       <h1>Meet up</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
   );
 }
